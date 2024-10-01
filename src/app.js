e12a4bfc26f593da35e32d325ec264e1d8e6961b
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-09-30 21:32:42 -0400

    Setup basic express app

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// src/app.js
+const express = require("express");
+const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+// TODO: Add routes here
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
